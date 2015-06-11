d0f6c8b44a092e317751c1d9845b8d10bcadc862
Hack Reactor Students <accounts@hackreactor.com>
2015-06-11 14:48:47 -0700

    refactored to use mongodb

--- app/models/user.js
+++ app/models/user.js
@@ -1,27 +1,28 @@
-// var db = require('../config');
-// var bcrypt = require('bcrypt-nodejs');
-// var Promise = require('bluebird');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var bluebird = require('bluebird');
 
-// var User = db.Model.extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function(){
-//     this.on('creating', this.hashPassword);
-//   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function(){
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
+var userSchema = mongoose.Schema({
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true }
+});
 
-// module.exports = User;
-// var mongoose =
-// var User = mongoose.model('User', userSchema);
+var User = mongoose.model('User', userSchema);
+
+User.comparePassword = function(candidatePassword, savedPassword, cb) {
+  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.pre('save', function(next){
+  var cipher = bluebird.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
+});
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./server-config.js');
 
 //var port = 4568;
-var port = process.env.port || 1337;
+var port = process.env.PORT || 1337;
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
